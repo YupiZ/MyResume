9c654858dbec8b79e4e371fed4498cc09aa4e2d6
YupiU <dozuikhunh@gmail.com>
2025-01-27 20:28:25 -0500

    Add initial styles and Button component; update App structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import "./App.css";
+import React, { useState } from "react";
+import Button from "./components/button";
+import "./styles/App.css";
 
 function App() {
+	const [count, setCount] = useState(0);
+
+	const clickHandler = () => {
+		setCount(count + 1);
+	};
+
 	return (
-		<>
-			<h1 className="text-3xl text-center text-red-700">Welcome to Vite with TailwindCSS and React</h1>
-		</>
+		<section className="h-screen flex justify-center items-center bg-slate-200">
+			<div className="flex flex-col gap-4 items-center">
+				<h1 className="text-3xl text-center text-red-700">Welcome to Vite with TailwindCSS and React</h1>
+				<Button onClick={clickHandler} children="Click Me" />
+				<p>Count: {count}</p>
+			</div>
+		</section>
 	);
 }
 
